585707010e519227a2bf231a52f4a26aabfe98e4
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-10-22 01:19:13 +0530

    Fix episode count on completed anime
    Set number of watched episodes as value '99999', so that MAL automatically sets the max possible watched episode count, which the the total number of available episodes for completed animes.

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ def EXPORT_TO_MAL(API_KEY, ID, watchlist_zoro):
             payload = {"status": convert[i]}
 
             if i == 'Completed':
-                payload["num_watched_episodes"] = -1
+                payload["num_watched_episodes"] = 99999
 
             response = requests.put(url,
                                     headers=headers,
